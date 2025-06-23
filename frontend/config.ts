4a95e11c50386161070e51db096f495e83295154
unknown <vermagroup44@gmail.com>
2025-06-23 09:07:24 +0530

    Updated config.ts

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_Backend = "https://dev-sketch-http-production.up.railway.app";
-export const WS_URL = "wss://dev-sketch-ws-production.up.railway.app";
+export const HTTP_Backend = "http://localhost:3000";
+export const WS_URL = "ws://localhost:8080";
